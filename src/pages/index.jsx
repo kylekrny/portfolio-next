802653d892c447bab8029d29ba61d120ebe1ebfe
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-05-10 09:06:45 -0600

    index page updated

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -12,15 +12,14 @@ import {
   LinkedInIcon,
   TwitterIcon,
 } from '@/components/SocialIcons'
-import logoAirbnb from '@/images/logos/airbnb.svg'
-import logoFacebook from '@/images/logos/facebook.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
-import logoStarbucks from '@/images/logos/starbucks.svg'
-import image1 from '@/images/photos/image-1.jpg'
-import image2 from '@/images/photos/image-2.jpg'
-import image3 from '@/images/photos/image-3.jpg'
-import image4 from '@/images/photos/image-4.jpg'
-import image5 from '@/images/photos/image-5.jpg'
+
+import logoLeadSherpa from '@/images/logos/lead-sherpa-logo.jpeg'
+import logoBoosterApps from '@/images/logos/booster-apps-logo.png'
+import image1 from '@/images/photos/coffee.jpg'
+import image2 from '@/images/photos/mountains.jpg'
+import image3 from '@/images/photos/amanda.jpg'
+import image4 from '@/images/photos/sunset.jpg'
+import image5 from '@/images/photos/maisy.jpg'
 import { formatDate } from '@/lib/formatDate'
 import { generateRssFeed } from '@/lib/generateRssFeed'
 import { getAllArticles } from '@/lib/getAllArticles'
@@ -139,35 +138,32 @@ function Newsletter() {
 function Resume() {
   let resume = [
     {
-      company: 'Planetaria',
-      title: 'CEO',
-      logo: logoPlanetaria,
+      company: 'Lead Sherpa',
+      title: 'Technical Product Owner',
+      logo: logoLeadSherpa,
+      start: '2022',
+      end: '2023',
+    },
+    {
+      company: 'Lead Sherpa',
+      title: 'Lead Front End Engineer',
+      logo: logoLeadSherpa,
+      start: '2021',
+      end: '2022',
+    },
+    {
+      company: 'Lead Sherpa',
+      title: 'Front End Engineer',
+      logo: logoLeadSherpa,
+      start: '2020',
+      end: '2021',
+    },
+    {
+      company: 'Booster Apps',
+      title: 'Front End Engineer',
+      logo: logoBoosterApps,
       start: '2019',
-      end: {
-        label: 'Present',
-        dateTime: new Date().getFullYear(),
-      },
-    },
-    {
-      company: 'Airbnb',
-      title: 'Product Designer',
-      logo: logoAirbnb,
-      start: '2014',
-      end: '2019',
-    },
-    {
-      company: 'Facebook',
-      title: 'iOS Software Engineer',
-      logo: logoFacebook,
-      start: '2011',
-      end: '2014',
-    },
-    {
-      company: 'Starbucks',
-      title: 'Shift Supervisor',
-      logo: logoStarbucks,
-      start: '2008',
-      end: '2011',
+      end: '2020',
     },
   ]
 
@@ -180,8 +176,8 @@ function Resume() {
       <ol className="mt-6 space-y-4">
         {resume.map((role, roleIndex) => (
           <li key={roleIndex} className="flex gap-4">
-            <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image src={role.logo} alt="" className="h-7 w-7" unoptimized />
+            <div className="relative mt-1 flex h-9 w-9 flex-none items-center justify-center shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+              <Image src={role.logo} alt="" className="h-9 w-9" unoptimized />
             </div>
             <dl className="flex flex-auto flex-wrap gap-x-2">
               <dt className="sr-only">Company</dt>
@@ -211,7 +207,7 @@ function Resume() {
           </li>
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
+      <Button href="/resume.pdf" variant="secondary" className="group mt-6 w-full">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
@@ -251,7 +247,7 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
+          Kyle Kearney - Front end developer
         </title>
         <meta
           name="description"
@@ -261,32 +257,24 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Front end developer, pretzel maker and workout buddy.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I'm Kyle, a front end developer based in Denver, Colorado. I have 6+ years of experience working on a wide range of projects. From small start ups to leading teams for multi-national conglomerates.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com"
-              aria-label="Follow on Twitter"
-              icon={TwitterIcon}
-            />
-            <SocialLink
-              href="https://instagram.com"
+              href="https://instagram.com/kyle.krny"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
             />
             <SocialLink
-              href="https://github.com"
+              href="https://github.com/kylekrny"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://linkedin.com/kylekrny"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
@@ -324,3 +312,11 @@ export async function getStaticProps() {
     },
   }
 }
+
+
+// How to do present for job experience
+
+      // end: {
+      //   label: 'Present',
+      //   dateTime: new Date().getFullYear(),
+      // },
