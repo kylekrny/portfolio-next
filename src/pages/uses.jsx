fecbefa7464007ad1d9accdf5db242b8e6bd8f15
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-05-10 13:17:58 -0600

    updated uses page

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -36,80 +36,55 @@ export default function Uses() {
         />
       </Head>
       <SimpleLayout
-        title="Software I use, gadgets I love, and other things I recommend."
-        intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+        title="My tools I use. functional. practical. probably second hand."
+        intro="For those looking to compare apples to apples - surprise you won! While my mouth salivates at the idea of a Herman Miller setup, saner heads pervail. For now..."
       >
         <div className="space-y-20">
           <ToolsSection title="Workstation">
-            <Tool title="16” MacBook Pro, M1 Max, 64GB RAM (2021)">
-              I was using an Intel-based 16” MacBook Pro prior to this and the
-              difference is night and day. I’ve never heard the fans turn on a
-              single time, even under the incredibly heavy loads I put it
-              through with our various launch simulations.
+            <Tool title="15” MacBook Pro, i7, 16gb ram (2017)">
+              I live by the <em>if it ain&apos;t broke don&apos;t fix it</em>{' '}
+              philosophy. Originally, a hand-me-down from my mom, I have spent the
+              last 3 years working with it, and hope to get a few more out of
+              it.
             </Tool>
-            <Tool title="Apple Pro Display XDR (Standard Glass)">
-              The only display on the market if you want something HiDPI and
-              bigger than 27”. When you’re working at planetary scale, every
-              pixel you can get counts.
+            <Tool title='LG UltaFine 5k 27"'>
+              This is one gorgeous monitor. As my fiance says "you can see every
+              freckles and crease in a person&apos;s skin." Originally,
+              purchased off craigslist it worked great for the first year. Now
+              it has a green line down the middle.
             </Tool>
-            <Tool title="IBM Model M SSK Industrial Keyboard">
-              They don’t make keyboards the way they used to. I buy these any
-              time I see them go up for sale and keep them in storage in case I
-              need parts or need to retire my main.
+            <Tool title='LG WQHD 34" ergo  Ultrawide'>
+              Comes with a built in monitor arm, cable management and decent
+              speakers. It is the perfect work horse for making those pixels
+              perfect.
             </Tool>
-            <Tool title="Apple Magic Trackpad">
-              Something about all the gestures makes me feel like a wizard with
-              special powers. I really like feeling like a wizard with special
-              powers.
+            <Tool title="Logitech MX Mini Keys">
+              While the old me considered switch colors like political parties.
+              The new me takes a simpler approach. Hot keys? check. tactile key
+              stroke? check. Long battery life? check.
             </Tool>
-            <Tool title="Herman Miller Aeron Chair">
-              If I’m going to slouch in the worst ergonomic position imaginable
-              all day, I might as well do it in an expensive chair.
+            <Tool title="Craigslist manual standing desk and office chair">
+              The desk creaks when you raise it, the chair provides some sort of
+              lumbar support, but I only paid $50.00 for both. Beggers
+              can&apos;t be choosers right?
             </Tool>
           </ToolsSection>
           <ToolsSection title="Development tools">
-            <Tool title="Sublime Text 4">
-              I don’t care if it’s missing all of the fancy IDE features
-              everyone else relies on, Sublime Text is still the best text
-              editor ever made.
+            <Tool title="VS Code">
+              Having struggled to close VIM enough times. VS Code allows me to
+              work any type of project, from VR to mobile to web. A true
+              craftsman never blames his tools.
             </Tool>
             <Tool title="iTerm2">
-              I’m honestly not even sure what features I get with this that
-              aren’t just part of the macOS Terminal but it’s what I use.
-            </Tool>
-            <Tool title="TablePlus">
-              Great software for working with databases. Has saved me from
-              building about a thousand admin interfaces for my various projects
-              over the years.
+              It adds some pretty colors and cool layouts. If you got any tips
+              or tricks for iTerm2 hit me up!
             </Tool>
           </ToolsSection>
           <ToolsSection title="Design">
             <Tool title="Figma">
-              We started using Figma as just a design tool but now it’s become
-              our virtual whiteboard for the entire company. Never would have
-              expected the collaboration features to be the real hook.
-            </Tool>
-          </ToolsSection>
-          <ToolsSection title="Productivity">
-            <Tool title="Alfred">
-              It’s not the newest kid on the block but it’s still the fastest.
-              The Sublime Text of the application launcher world.
-            </Tool>
-            <Tool title="Reflect">
-              Using a daily notes system instead of trying to keep things
-              organized by topics has been super powerful for me. And with
-              Reflect, it’s still easy for me to keep all of that stuff
-              discoverable by topic even though all of my writing happens in the
-              daily note.
-            </Tool>
-            <Tool title="SavvyCal">
-              Great tool for scheduling meetings while protecting my calendar
-              and making sure I still have lots of time for deep work during the
-              week.
-            </Tool>
-            <Tool title="Focus">
-              Simple tool for blocking distracting websites when I need to just
-              do the work and get some momentum going.
+              I have used a variety of design tools, but my personal favorite is
+              figma. A good free tier for the solo guy like myself, but with
+              enough features to handle even the largest projects.
             </Tool>
           </ToolsSection>
         </div>
