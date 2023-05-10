ec257dce7d85e7ccc72abd3fc35727fb7b1a8dc9
Kyle <45010713+kylekrny@users.noreply.github.com>
2023-05-10 11:38:57 -0600

    update about section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -61,35 +61,39 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Spencer Sharp. I live in New York City, where I design the
-              future.
+              I’m Kyle Kearney. I write clean code, make pretzels and build
+              Legos.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I am a front end engineer with 6+ years of experience. I have
+                worked in a wide array of team environments, tech stacks and
+                application types. From leading the web marketing effort at a
+                multi billion dollar conglomerate, to wearing all the hats at
+                small start-ups.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                I spent two and a half years growing at Lead Sherpa. I spent the
+                time perfecting my craft with React and learning the ins and out
+                of managing a product and more importantly building a team. My
+                role shifted from from frontend engineer, to lead engineer,
+                ending with technical product owner. While I do enjoy the
+                business aspect of the product role, solving problems and
+                working along side my fellow engineer truly brings me great joy.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                Minimalism and simplicity are key values in every aspect of my
+                life, especially development. Clean, readable code and quality
+                test coverage are the fundamental attributes to a quality
+                application. I primarily code in Javascript and Ruby, but have
+                experience with a few other languages.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                When I am not working you can find me, cooking, learning or
+                training for my first triathlon. I am a life long learner, AFOL
+                and personal finance nerd, You can checkout my current
+                projects on my GitHub. If you have a question, looking for
+                a dev, or a friend feel free to reach out.
               </p>
             </div>
           </div>
